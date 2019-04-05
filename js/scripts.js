fccde3067a5e5597fcf387547faa1ff2884f344c
Stephen Trewick <stephent818@gmail.com>
2019-04-05 13:33:16 -0700

    show initial logic for finding multi-digit numbers

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,20 @@ $(document).ready(function(){
     var boop = "Boop!";
     var sorry = "Im sorry Dave, Im afraid I cannot do that."
     var array = [];
+    var three = [3];
+    var two = [2];
+    var one = [1];
+    var value;
     var finalNum = $("input#inputNumber").val();
 
-    for (var i = 0; i <= finalNum; i++){
+    for (var i = 0; i <= finalNum; i++) {
       array.push(i);
-      if (array[i] === 1 || array[i] === 2 || array[i] === 3) {
+      if (array[i] > 9) {
+        var convert = i.toString();
+        console.log(convert);
+      }
+
+     if (array[i] === 1 || array[i] === 2 || array[i] === 3) {
         if (array[i] === 3) {
           array[i] = sorry;
         } else if (array[i] === 2) {
@@ -20,6 +29,7 @@ $(document).ready(function(){
       }
     }
 
+
     console.log(array);
 
 
@@ -27,7 +37,7 @@ $(document).ready(function(){
 
     var halsAnswer = array.toString();
 
-    $("#output").append(halsAnswer);
+    $("#output").text(halsAnswer);
   });
   $("#refresh").click(function(){
     location.reload();
