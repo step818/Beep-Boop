eb66eafcf52057fd6d7ccb09d827de154e4aa1cd
Stephen Trewick <stephent818@gmail.com>
2019-04-05 11:42:53 -0700

    return the exceptions for 1,2, and 3, but not for multi-digit numbers containing 1,2, or 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,29 @@
 $(document).ready(function(){
   $("form#hal9000").submit(function(event){
     event.preventDefault();
+    var beep = "Beep!";
+    var boop = "Boop!";
+    var sorry = "Im sorry Dave, Im afraid I cannot do that."
     var array = [];
-    var finalNum = parseInt($("input#inputNumber").val());
-    console.log(finalNum);
+    var finalNum = $("input#inputNumber").val();
 
     for (var i = 0; i <= finalNum; i++){
       array.push(i);
+      if (array[i] === 1 || array[i] === 2 || array[i] === 3) {
+        if (array[i] === 3) {
+          array[i] = sorry;
+        } else if (array[i] === 2) {
+          array[i] = boop;
+        } else {
+          array[i] = beep;
+        }
+      }
     }
+
     console.log(array);
+
+
+
 
     var halsAnswer = array.toString();
 
