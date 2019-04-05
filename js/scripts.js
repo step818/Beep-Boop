c180fe3b5f5ef71cf52a19b41d42f8933a3204bc
Stephen Trewick <stephent818@gmail.com>
2019-04-05 10:48:59 -0700

    make users input number in to an array with numbers starting with 0 up to that number

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
 $(document).ready(function(){
   $("form#hal9000").submit(function(event){
     event.preventDefault();
-
+    var array = [];
     var finalNum = parseInt($("input#inputNumber").val());
     console.log(finalNum);
 
-    $("#output").append(finalNum);
+    for (var i = 0; i <= finalNum; i++){
+      array.push(i);
+    }
+    console.log(array);
+
+    var halsAnswer = array.toString();
+
+    $("#output").append(halsAnswer);
   });
   $("#refresh").click(function(){
     location.reload();
