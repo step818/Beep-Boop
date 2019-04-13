1e6d3cb040d0309d08ea966e4ee44253811fcaa4
Stephen Trewick <stephent818@gmail.com>
2019-04-13 12:54:44 -0700

    show separated user interface and business logic in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,23 @@
+//User Interface Logic----
 $(document).ready(function(){
   $("form#hal9000").submit(function(event){
     event.preventDefault();
+    var finalNum = $("input#inputNumber").val();
+    var array = [];
+    function output(array) {
+      var halsAnswer = array.toString();
+      $("#output").text(halsAnswer);
+    }
+
+    $("#refresh").click(function(){
+      location.reload();
+    });
+
+
+//Business Logic----
     var beep = " Beep!";
     var boop = " Boop!";
     var sorry = " I'm sorry "+ $("input#userName").val() +", I'm afraid I can't do that"
-    var array = [];
-    var finalNum = $("input#inputNumber").val();
 
     for (var i = 0; i <= finalNum; i++) {
       array.push(i);
@@ -15,23 +27,16 @@ $(document).ready(function(){
         var contains1 = multiDigit.includes("1");
 
         if (contains3) {
-          console.log("sorry");
+          //console.log("sorry");
           array.splice(i,1,sorry);
         } else if (contains2) {
-          console.log("boop");
+          //console.log("boop");
           array.splice(i,1,boop);
         } else if (contains1) {
-          console.log("beep");
+          //console.log("beep");
           array.splice(i,1,beep);
         }
       }
-  
-
-    var halsAnswer = array.toString();
-
-    $("#output").text(halsAnswer);
-  });
-  $("#refresh").click(function(){
-    location.reload();
+      output(array);
   });
 });
